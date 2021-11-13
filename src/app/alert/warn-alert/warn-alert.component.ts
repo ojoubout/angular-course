f5cf3acfdcd64e2d7732599d66cce1b294d53056
ojoubout <oussamajoubouti@gmail.com>
2021-11-13 13:30:27 +0100

    init and finish

---
+++ src/app/alert/warn-alert/warn-alert.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'warn-alert',
+  templateUrl: 'warn-alert.component.html',
+  styleUrls: ['warn-alert.component.css', '../alert.component.css']
+})
+export class WarnAlertComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {WarnAlertComponent} from "./alert/warn-alert/warn-alert.component";
+import { SuccessAlertComponent } from './alert/success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarnAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
